b0c903148c10dc4919f81f7e4a879376011bcd23
FranziGrieser <franzi.grieser@gmail.com>
2020-01-01 14:55:54 +0100

    refactor condtionals and use I18n

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,19 +2,14 @@
 
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: "users/result" }
-        end
-      else
-        flash[:alert] = "You have entered an incorrect symbol"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:alert] = I18n.t("search.empty")
     else
-      redirect_to my_portfolio_path
-      flash[:alert] = "You have entered an empty search"
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:alert] = I18n.t("search.incorrect_symbol") unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: "users/result" }
     end
   end
 end
