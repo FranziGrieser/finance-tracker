bfb067c6dde6504f8ed3829df0da0fe02cf1a75f
FranziGrieser <franzi.grieser@gmail.com>
2020-01-02 22:09:44 +0100

    add destroy action for user stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,4 +12,14 @@ class UserStocksController < ApplicationController
       I18n.t("flash.user_stock_added", stock_ticker: @user_stock.stock.name)
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    @user_stock =
+      UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    @user_stock.destroy
+    flash[:notice] =
+      I18n.t("flash.user_stock_deleted")
+    redirect_to my_portfolio_path
+  end
 end
