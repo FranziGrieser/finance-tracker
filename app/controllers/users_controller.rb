27e5f78b5d5bdb603be207a3664449ae5035e738
FranziGrieser <franzi.grieser@gmail.com>
2020-01-02 22:12:40 +0100

    add instance variables for user stocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 # frozen-string-literal: true
 
 class UsersController < ApplicationController
-  def my_portfolio; end
+  def my_portfolio
+    @user = current_user
+    @user_stocks = current_user.stocks
+  end
 end
