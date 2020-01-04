63a594638854e63bc5ad979cbf686741449da7e6
FranziGrieser <franzi.grieser@gmail.com>
2020-01-04 20:17:20 +0100

    add my friends template with routes and nav-link

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
+
   root "welcome#index"
   get "pages/about"
   get "pages/contact"
   get "pages/projects"
   get "my_portfolio", to: "users#my_portfolio"
   get "search_stocks", to: "stocks#search"
-  resources :user_stocks, only: [:create, :destroy]
+  get "my_friends", to: "users#my_friends"
 end
